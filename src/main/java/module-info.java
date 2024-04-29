c0feb6b6e0f41da3c9f368edd1a8848acac760b6
Helene Harrer <if22b036@technikum-wien.at>
2024-04-30 01:42:45 +0200

    basic 2 way data binding: adding new tours to table

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,10 @@ module technikum.at.tourplanner_swen2_team5 {
     requires javafx.fxml;
     requires lombok;
 
-
     opens technikum.at.tourplanner_swen2_team5 to javafx.fxml;
     exports technikum.at.tourplanner_swen2_team5;
     exports technikum.at.tourplanner_swen2_team5.controller;
     opens technikum.at.tourplanner_swen2_team5.controller to javafx.fxml;
+
+    opens technikum.at.tourplanner_swen2_team5.models to javafx.base, javafx.fxml;
 }
--- src/main/java/technikum/at/tourplanner_swen2_team5/controller/TourListController.java
+++ src/main/java/technikum/at/tourplanner_swen2_team5/controller/TourListController.java
@@ -1,4 +1,37 @@
 package technikum.at.tourplanner_swen2_team5.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import technikum.at.tourplanner_swen2_team5.models.Tour;
+import technikum.at.tourplanner_swen2_team5.viewmodels.TourViewModel;
+
 public class TourListController {
+    @FXML private TableView<Tour> toursTable;
+    @FXML private TableColumn<Tour, String> colType;
+    @FXML private TableColumn<Tour, String> colName;
+    @FXML private TableColumn<Tour, String> colStart;
+    @FXML private TableColumn<Tour, String> colDestination;
+    @FXML private TableColumn<Tour, String> colDistance;
+    @FXML private TableColumn<Tour, String> colTime;
+
+    private TourViewModel viewModel;
+
+    public void initialize() {
+        viewModel = TourViewModel.getInstance();
+
+        // Hier werden die Zellen der Spalten an die Properties gebunden
+        colType.setCellValueFactory(new PropertyValueFactory<>("transportType"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colStart.setCellValueFactory(new PropertyValueFactory<>("from"));
+        colDestination.setCellValueFactory(new PropertyValueFactory<>("to"));
+        // colDistance.setCellValueFactory(new PropertyValueFactory<>("distance"));
+        // colTime.setCellValueFactory(new PropertyValueFactory<>("time"));
+
+        toursTable.setItems(viewModel.getTours());
+    }
 }
+
+
+
