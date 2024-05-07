98be104b3c880d9cd77a6043345c39be72c5297c
Helene Harrer <if22b036@technikum-wien.at>
2024-05-07 19:21:20 +0200

    added buttons to table

--- src/main/java/technikum/at/tourplanner_swen2_team5/controller/TourListController.java
+++ src/main/java/technikum/at/tourplanner_swen2_team5/controller/TourListController.java
@@ -1,12 +1,15 @@
 package technikum.at.tourplanner_swen2_team5.controller;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import technikum.at.tourplanner_swen2_team5.MainTourPlaner;
 import technikum.at.tourplanner_swen2_team5.models.TourModel;
 import technikum.at.tourplanner_swen2_team5.viewmodels.TourViewModel;
@@ -23,6 +26,8 @@ public class TourListController {
     @FXML private TableColumn<TourModel, String> colDestination;
     @FXML private TableColumn<TourModel, String> colDistance;
     @FXML private TableColumn<TourModel, String> colTime;
+    @FXML private TableColumn<TourModel, Void> colButtons;
+
 
     private TourViewModel viewModel;
 
@@ -61,8 +66,55 @@ public class TourListController {
         // colDistance.setCellValueFactory(new PropertyValueFactory<>("distance"));
         // colTime.setCellValueFactory(new PropertyValueFactory<>("time"));
 
+        colButtons.setCellFactory(column -> new TableCell<TourModel, Void>() {
+            private final HBox buttonContainer = new HBox(10);
+            private final Button editButton = createButton("edit", "#A4D65E", "#395C37", false);
+            private final Button detailButton = createButton("detail", "#A4D65E", "#395C37", false);
+            private final Button downloadButton = createButton("download", "#A4D65E", "#395C37", false);
+            private final Button trashButton = createButton("trash", "#F44336", "#BB1B11", true);
+
+            {
+                buttonContainer.setAlignment(Pos.CENTER_RIGHT);
+                buttonContainer.getChildren().addAll(editButton, detailButton, downloadButton, trashButton);
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(buttonContainer);
+                }
+            }
+        });
+
+
         toursTable.setItems(viewModel.getTours());
     }
+
+    private Button createButton(String iconName, String baseColor, String hoverColor, boolean isTrashButton) {
+        Button button = new Button();
+        button.getStyleClass().add("icon-button-small"); // Standard CSS-Klasse
+
+        String imageName = "img/icons/" + iconName.toLowerCase() + "-icon.png";
+        URL resource = MainTourPlaner.class.getResource(imageName);
+        Image icon = new Image(resource.toString());
+        ImageView iconView = new ImageView(icon);
+        iconView.setFitWidth(20);
+        iconView.setFitHeight(20);
+        button.setGraphic(iconView);
+
+        String borderColor = isTrashButton ? "#F44336" : "black";
+        String radius = "50";
+        button.setStyle(String.format("-fx-background-color: %s; -fx-border-color: #202020; -fx-border-radius: %s; -fx-background-radius: %s;", baseColor, radius, radius));
+
+        button.setOnMouseEntered(e -> button.setStyle(String.format("-fx-background-color: %s; -fx-border-color: %s; -fx-border-radius: %s; -fx-background-radius: %s;", hoverColor, borderColor, radius, radius)));
+        button.setOnMouseExited(e -> button.setStyle(String.format("-fx-background-color: %s; -fx-border-color: %s; -fx-border-radius: %s; -fx-background-radius: %s;", baseColor, borderColor, radius, radius)));
+
+        return button;
+    }
+
 }
 
 
