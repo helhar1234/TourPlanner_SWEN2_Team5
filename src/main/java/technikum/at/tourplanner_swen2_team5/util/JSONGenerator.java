34bbb5d148c4ce429c92bcd39e18d8106327ac6b
Helene <115228353+helhar1234@users.noreply.github.com>
2024-06-30 15:10:19 +0200

    Update JSONGenerator.java

--- src/main/java/technikum/at/tourplanner_swen2_team5/util/JSONGenerator.java
+++ src/main/java/technikum/at/tourplanner_swen2_team5/util/JSONGenerator.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import technikum.at.tourplanner_swen2_team5.BL.models.TourLogModel;
 import technikum.at.tourplanner_swen2_team5.BL.models.TourModel;
 import technikum.at.tourplanner_swen2_team5.BL.validation.TourLogValidationService;
@@ -20,25 +18,14 @@ import java.io.IOException;
 import java.util.List;
 
 @Slf4j
-@Component
 public class JSONGenerator {
     private ObjectMapper mapper;
 
-    private final TourViewModel tourViewModel;
-    private final TourLogViewModel tourLogViewModel;
-    private final TourValidationService tourValidationService;
-    private final TourLogValidationService tourLogValidationService;
-
-    public JSONGenerator(TourViewModel tourViewModel, TourLogViewModel tourLogViewModel, TourValidationService tourValidationService, TourLogValidationService tourLogValidationService) {
-        this.tourViewModel = tourViewModel;
-        this.tourLogViewModel = tourLogViewModel;
-        this.tourValidationService = tourValidationService;
-        this.tourLogValidationService = tourLogValidationService;
+    public JSONGenerator() {
         mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
 
-    // Diese Methode handhabt alles: Generieren, Datei auswählen und Speichern
     public void generateTourExportsJSON(TourModel tour, List<TourLogModel> tourLogs) {
         String jsonContent = generateJSON(tour, tourLogs);
         if (jsonContent != null) {
@@ -49,18 +36,16 @@ public class JSONGenerator {
         }
     }
 
-    // Hilfsmethode zur Generierung des JSON-Strings
     private String generateJSON(TourModel tour, List<TourLogModel> tourLogs) {
         try {
             TourExport export = new TourExport(tour, tourLogs);
             return mapper.writeValueAsString(export);
         } catch (IOException e) {
-            System.out.println("Error generating JSON: " + e.getMessage());
+            log.error("Error generating JSON: {}", e.getMessage());
             return null;
         }
     }
 
-    // Verwendung des nativen FileDialogs
     private File chooseFileLocation(String id) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setInitialFileName(id + "-tour.json");
@@ -75,17 +60,15 @@ public class JSONGenerator {
         }
     }
 
-    // Methode zum Speichern des JSON in der Datei
     private void saveJSONToFile(String jsonContent, File file) {
         try (FileWriter fileWriter = new FileWriter(file)) {
             fileWriter.write(jsonContent);
-            System.out.println("Saved JSON to " + file.getPath());
+            log.info("Saved JSON to {}", file.getPath());
         } catch (IOException e) {
-            System.out.println("Error saving JSON to file: " + e.getMessage());
+            log.error("Error saving JSON to file: {}", e.getMessage());
         }
     }
 
-    // Innere Klasse zur Bündelung von Tour und deren Logs
     public static class TourExport {
         public TourModel tour;
         public List<TourLogModel> logs;
@@ -98,11 +81,14 @@ public class JSONGenerator {
         }
     }
 
-    // ObjectMapper-Instanz für das Parsing von JSON
     private static ObjectMapper objectMapper = new ObjectMapper();
 
+<<<<<<< Updated upstream
     // Methode, um den File Explorer zu öffnen und JSON hochzuladen
+    public static void uploadJSON(Stage stage) throws IOException {
+=======
     public void uploadJSON(Stage stage) throws IOException {
+>>>>>>> Stashed changes
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JSON Files", "*.json"));
@@ -110,17 +96,23 @@ public class JSONGenerator {
 
         if (selectedFile != null) {
             TourExport tourExport = objectMapper.readValue(selectedFile, TourExport.class);
+            TourViewModel tourViewModel = TourViewModel.getInstance();
+            TourValidationService tourValidationService = new TourValidationService();
             if (tourValidationService.validateTour(tourExport.tour).isEmpty() && tourValidationService.validateNameExists(tourExport.tour.getName(), null).isEmpty()){
                 String id = tourViewModel.addTour(tourExport.tour);
+                TourLogViewModel tourLogViewModel = new TourLogViewModel();
+                TourLogValidationService tourLogValidationService = new TourLogValidationService();
                 for (TourLogModel log : tourExport.logs) {
                     if (tourLogValidationService.validateTourLog(log).isEmpty()){
                         log.setTour(tourViewModel.getTourById(id));
                         tourLogViewModel.addTourLogFromUpload(log);
                     }
                 }
+
             } else {
                 throw new IOException();
             }
+
         }
     }
 }
